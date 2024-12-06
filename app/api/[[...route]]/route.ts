d4bc2f23b253ca02c63a9ea43f91abef12bf9827
Dayane <dayane.rodrigues@bcrcx.com>
2024-12-06 12:44:13 -0300

    feat: implement summary endpoint with current and last period calculations

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel'
 
+import summary from './summary';
 import accounts from './accounts';
 import categories from './categories';
-import  transactions  from './transactions';
+import transactions from './transactions';
 
 export const runtime = 'edge'
 
@@ -11,6 +12,7 @@ const app = new Hono().basePath('/api')
 
 
 const routes = app
+    .route('/summary', summary)
     .route('/accounts', accounts)
     .route('/categories', categories)
     .route('/transactions', transactions)

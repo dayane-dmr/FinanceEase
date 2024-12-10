fbb1ddb98dc12bb6d2e6e5977c0c5ca2975ac8b8
Dayane <dayane.rodrigues@bcrcx.com>
2024-12-10 14:08:57 -0300

    chore: remove unused imports to improve code clarity

--- components/filters.tsx
+++ components/filters.tsx
@@ -4,8 +4,8 @@ import { DateFilter } from "./date-filter"
 export const Filters = () => {
     return(
         <div className='flex flex-col lg:flex-row-reverse relative bottom-auto lg:bottom-[50px] items-center gap-y-2 lg:gap-y-0 lg:gap-x-2'>
-            <AccountFilter />
             <DateFilter />
+            <AccountFilter />
         </div>
     )
 }
